51d4058fc483674128a05fb8d3b625cd63830977
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-01 21:36:04 +0900

    feat: imgCard 컴포넌트 제작

---
+++ packages/ui/src/ImgCard/imgCard.stories.tsx
@@ -0,0 +1,42 @@
+import ImgCard from '.';
+
+const meta = {
+  title: 'card/ImgCard',
+  component: ImgCard,
+  argTypes: {
+    label: {
+      control: {type: 'text'},
+    },
+    price: {
+      control: {type: 'text'},
+    },
+    onClick: {
+      control: {type: 'boolean'},
+    },
+  },
+};
+
+export default meta;
+
+export const 이미지_카드 = ({
+  label = 'Bouquets Fresh Flowers',
+  price = '70',
+  onClick,
+}: {
+  label: string;
+  price: string;
+  onClick: boolean;
+}) => {
+  const handleClick = () => {
+    alert('클릭');
+  };
+
+  return (
+    <ImgCard
+      label={<ImgCard.Label>{label}</ImgCard.Label>}
+      price={<ImgCard.Price>{price}</ImgCard.Price>}
+      src="https://picsum.photos/1000/1000"
+      onClick={onClick ? handleClick : undefined}
+    />
+  );
+};
