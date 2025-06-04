f7238c004e756e7445c9d7b37a32521df8604940
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-04 10:41:47 +0900

    feat: NavButton 컴포넌트 제작

---
+++ packages/ui/src/NavButton/navButton.stories.tsx
@@ -0,0 +1,38 @@
+import IconButton from '../IconButton';
+import NavButton from '.';
+import Menu from '../assets/icon/menu.svg';
+
+const meta = {
+  title: 'nav/NavButton',
+  component: NavButton,
+  argTypes: {
+    text: {
+      control: {type: 'text'},
+    },
+    isIcon: {
+      control: {type: 'boolean'},
+    },
+    isBorderRight: {
+      control: {type: 'boolean'},
+    },
+  },
+};
+
+export default meta;
+
+export const 네비게이션_버튼 = ({
+  text = 'Link',
+  isIcon,
+  isBorderRight,
+}: {
+  text: string;
+  isIcon: boolean;
+  isBorderRight: boolean;
+}) => {
+  return (
+    <NavButton
+      isBorderRight={isBorderRight}
+      nav={isIcon ? <IconButton icon={Menu} /> : <NavButton.TextButton>{text}</NavButton.TextButton>}
+    />
+  );
+};
