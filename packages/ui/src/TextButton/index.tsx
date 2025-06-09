7b350a5f591b84b85f2ab2c8799e2c4a83637159
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-10 00:13:01 +0900

    refactor: SVG 컴포넌트 적용하여 수정

--- packages/ui/src/TextButton/index.tsx
+++ packages/ui/src/TextButton/index.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './textButton.module.css';
+import SVG from '../SVG';
 
 const cx = classNames.bind(styles);
 
@@ -12,9 +13,9 @@ export interface TextButtonProps extends React.ButtonHTMLAttributes<HTMLButtonEl
 export default function TextButton({children, leftIcon, rightIcon, ...props}: TextButtonProps) {
   return (
     <button className={cx('button', 'typography-body', {'button-with-icon': leftIcon || rightIcon})} {...props}>
-      {leftIcon ? <img src={leftIcon} alt="icon" aria-hidden width={24} height={24} /> : null}
+      {leftIcon ? <SVG src={leftIcon} /> : null}
       {children}
-      {rightIcon ? <img src={rightIcon} alt="icon" aria-hidden width={24} height={24} /> : null}
+      {rightIcon ? <SVG src={rightIcon} /> : null}
     </button>
   );
 }
