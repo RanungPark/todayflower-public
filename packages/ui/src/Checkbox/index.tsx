c571e954688e71d4107fd68c107438a5df489c21
_wo_.ong <ckehfqkr302@gmail.com>
2025-06-11 12:44:24 +0900

    refactor: 파일 명 오류로 경로 문제 해결

--- packages/ui/src/Checkbox/index.tsx
+++ packages/ui/src/Checkbox/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import styles from './Checkbox.module.css';
+import styles from './checkbox.module.css';
 import Check from '../assets/icon/check.svg';
 import SVG from '../SVG';
 
